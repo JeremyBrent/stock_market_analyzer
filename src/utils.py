d820da6281585e7bd8f57643a27e43e06bd072a0
Jeremy Brent <j.brent@ieee.org>
2024-09-25 00:41:57 -0400

    added method to get what i call the effective date of a news article

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,42 @@
-import datetime
+import datetime as dt
 from src.consts import DATE_FORMAT
 
 
 class Utils:
 
     @staticmethod
-    def convert_datetime(timestamp: int) -> str:
+    def convert_datetime(timestamp: int) -> tuple[str, str]:
         """
-        Given an int timestamp, convert to
+        This method will extract a human-readable date and determine for which day this news
+        might impact stock prices. So for example, if the news comes from after 4pm on Tuesday,
+        we might say that it will have an impact on trading Wednesday.
+
         :param timestamp:
         :return:
         """
         # Convert to datetime object
-        dt_object = datetime.datetime.fromtimestamp(timestamp)
-        # Convert to string
-        datetime_str = dt_object.strftime(DATE_FORMAT)
+        original_date = dt.datetime.fromtimestamp(timestamp)
+        dt_object = original_date
 
-        return datetime_str
+        # Define 4 PM as the time to compare against
+        four_pm = original_date.replace(hour=16, minute=0, second=0, microsecond=0)
+
+        # If the input time is after 4 PM, move to the next day
+        if original_date > four_pm:
+            dt_object = original_date + dt.timedelta(days=1)
+
+            # Reset time, we are throwing time out
+            dt_object = dt_object.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        # Ensure the date is a weekday (Monday to Friday)
+        if dt_object.weekday() == 5:  # Saturday
+            dt_object += dt.timedelta(days=2)  # Move to Monday
+        elif dt_object.weekday() == 6:  # Sunday
+            dt_object += dt.timedelta(days=1)  # Move to Monday
+        elif dt_object.weekday() == 4 and dt_object > four_pm:  # Friday after 4 PM
+            dt_object += dt.timedelta(days=3)  # Move to Monday
+
+        datetime_str = dt_object.strftime(DATE_FORMAT).split(' ')[0]
+        original_date_str = original_date.strftime(DATE_FORMAT)
+
+        return original_date_str, datetime_str
---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+import unittest
+from src.utils import Utils
+
+
+class TestUtils(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.utils = Utils()
+
+    def test_convert_datetime(self):
+        test_data = {
+            1727104000: ('2024-09-23 11:06:40', '2024-09-23'),
+            1727130000: ('2024-09-23 18:20:00', '2024-09-24'),  # after 4pm, move to next day
+            1727490000:  ('2024-09-27 22:20:00', '2024-09-30'),  # Friday after 4pm move to Monday
+            1727500000: ('2024-09-28 01:06:40', '2024-09-30'),  # Saturday to Monday
+            1727590000: ('2024-09-29 02:06:40', '2024-09-30'),  # Sunday move to Monday
+        }
+
+        for input, expected_data in test_data.items():
+            actual_output = self.utils.convert_datetime(input)
+            self.assertEqual(actual_output, expected_data)
