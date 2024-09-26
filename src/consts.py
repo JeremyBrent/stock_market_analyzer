6cf36b4308943a7ca42fb2c5d9c355500f3330d4
Jeremy Brent <j.brent@ieee.org>
2024-09-26 00:34:58 -0400

    added proj root path to consts

--- src/consts.py
+++ src/consts.py
@@ -1,3 +1,7 @@
+import os
+
+PROJECT_ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+
 CPU_COUNT: int = 8
 PARALLEL_CHUNK_SIZE: int = 5
 DATE_FORMAT: str = '%Y-%m-%d %H:%M:%S'
