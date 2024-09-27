f31699cc9185216f3d63de481c0fc6ef2696e012
Jeremy Brent <j.brent@ieee.org>
2024-09-27 02:49:01 -0400

    added random state const

--- src/consts.py
+++ src/consts.py
@@ -5,3 +5,5 @@ PROJECT_ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir)
 CPU_COUNT: int = 8
 PARALLEL_CHUNK_SIZE: int = 5
 DATE_FORMAT: str = '%Y-%m-%d %H:%M:%S'
+
+RANDOM_STATE: int = 42
